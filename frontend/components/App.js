6c5115cab8acfd8f9d47a6cc609ebb95228b776b
Harrison Snellback <harrison.snellback@gmail.com>
2022-11-08 04:49:45 -0600

    Added in all DOM

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React from 'react';
+import Form from './Form';
+import Todo from './Todo';
+import TodoList from './TodoList';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: ''
+    }
+  }
+
   render() {
     return (
-      <div>
-        Todo App
+      <div className="App">
+        <h1>Todos:</h1>
+        <ul>
+          <li>Walk the dog</li>
+          <li>Take out trash</li>
+          <li>Workout</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
